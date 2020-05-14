fcecb553cf207f4cd14b2dcbff0d4b49d82ce7a7
johnrohan87 <john.rohan87@gmail.com>
2020-05-14 17:34:15 +0000

    updating files

--- src/app.py
+++ src/app.py
@@ -1,2 +1,18 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+#this must be at the EOF
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
